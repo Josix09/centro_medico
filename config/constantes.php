57e3f53261d5eed6539dbee46fe86b7c5d289300
Josix09 <jorgesilva@colegioguanenta.edu.co>
2026-05-19 09:19:41 -0500

    up

---
+++ config/constantes.php
@@ -0,0 +1,15 @@
+<?php
+// parametros requeridos para la conexion con la BD 
+
+    // parametros BD local - Constantes
+    define('USER', 'j_silva'); // crea la constante USER con valor 'root'
+    define('PW', '12345');
+    define('HOST', 'localhost');
+    define('BD', 'centro_medico');
+
+    // parametros BD remota (infinityfree)
+    /*define('USER', 'if0_41717667'); // crea la constante USER con valor 'root'
+    define('PW', 'QbZrIFeNu3Hg2');
+    define('HOST', 'sql105.infinityfree.com');
+    define('BD', 'if0_41717667_centro_medico');*/
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>centro_medico</title>
+</head>
+<body>
+    <h1>centro medico(chite perro, chite gripa)</h1>
+    <h2>Jorge Luis Silva Morales</h2>
+</body>
+</html>
+
